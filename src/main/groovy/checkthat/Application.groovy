d590dcc00142da7d079719af69e1eacf88439e44
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2016-03-23 19:10:53 -0700

    Support for self-signed SSL

--- src/main/groovy/checkthat/Application.groovy
+++ src/main/groovy/checkthat/Application.groovy
@@ -1,13 +1,18 @@
 package checkthat
 
 import org.apache.http.client.config.RequestConfig
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy
 import org.apache.http.impl.client.CloseableHttpClient
 import org.apache.http.impl.client.HttpClients
+import org.apache.http.ssl.SSLContexts
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.core.env.Environment
+
+import javax.net.ssl.SSLContext
 
 /**
  * @author Jonatan Ivanov
@@ -18,15 +23,26 @@ class Application {
 
     @Bean
     CloseableHttpClient httpClient() {
-        RequestConfig requestConfig = RequestConfig.custom()
+        return HttpClients.custom()
+                .setDefaultRequestConfig(createRequestConfig())
+                .setSSLSocketFactory(createSSLConnectionSocketFactory())
+                .build();
+    }
+
+    private RequestConfig createRequestConfig() {
+        return RequestConfig.custom()
                 .setSocketTimeout(env.getProperty("http.socketTimeout", Integer.class))
                 .setConnectTimeout(env.getProperty("http.connectTimeout", Integer.class))
                 .setConnectionRequestTimeout(env.getProperty("http.connectionRequestTimeout", Integer.class))
                 .build();
+    }
 
-        return HttpClients.custom()
-                .setDefaultRequestConfig(requestConfig)
+    private SSLConnectionSocketFactory createSSLConnectionSocketFactory() {
+        SSLContext sslContext = SSLContexts.custom()
+                .loadTrustMaterial(null, new TrustSelfSignedStrategy())
                 .build();
+
+        return new SSLConnectionSocketFactory(sslContext, SSLConnectionSocketFactory.getDefaultHostnameVerifier());
     }
 
     static void main(String[] args) {
