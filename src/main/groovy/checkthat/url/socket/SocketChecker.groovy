c7c15869346042dc7fc4e6c11e79769a9f12c86b
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2016-03-27 13:06:51 -0700

    Define timeout for SocketChecker, adjust timeout settings

--- src/main/groovy/checkthat/url/socket/SocketChecker.groovy
+++ src/main/groovy/checkthat/url/socket/SocketChecker.groovy
@@ -3,6 +3,7 @@ package checkthat.url.socket
 import checkthat.error.CheckerException
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 
 import java.util.function.BiFunction
@@ -14,13 +15,16 @@ import java.util.function.BiFunction
 class SocketChecker implements BiFunction<String, Integer, SocketResponse> {
     private static final Logger LOGGER = LoggerFactory.getLogger(SocketChecker.class);
 
+    @Value('${socket.timeout}') private int socketTimeout;
+
     @Override
     SocketResponse apply(String host, Integer port) {
         LOGGER.info("Connecting to $host:$port");
 
         Socket socket = null;
         try {
-            socket = new Socket(host, port);
+            socket = new Socket();
+            socket.connect(new InetSocketAddress(host, port), socketTimeout);
             LOGGER.info("Successfully connected to $host:$port");
             return new SocketResponse(
                     host: host,
