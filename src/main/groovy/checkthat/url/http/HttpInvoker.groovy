ebbe9de81814ab8a258d74d4ac76f1c4e4017910
Spencer Thomas <spthomas@expedia.com>
2018-01-23 13:45:25 -0800

    Add thread-level execution timeout to HTTP requests to enforce strict timeout

--- src/main/groovy/checkthat/url/http/HttpInvoker.groovy
+++ src/main/groovy/checkthat/url/http/HttpInvoker.groovy
@@ -1,44 +1,20 @@
 package checkthat.url.http
 
-import checkthat.error.CheckerException
-import org.apache.http.StatusLine
-import org.apache.http.client.methods.CloseableHttpResponse
-import org.apache.http.client.methods.HttpGet
 import org.apache.http.impl.client.CloseableHttpClient
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
+
 
 /**
  * @author Jonatan Ivanov
  */
 @Component
 class HttpInvoker {
-    private static final Logger LOGGER = LoggerFactory.getLogger(HttpInvoker.class);
-    @Autowired private CloseableHttpClient httpClient;
+    @Autowired private CloseableHttpClient httpClient
+    @Value('${http.executionTimeout}') private int httpRequestTimeout
 
     HttpResponse invoke(String url) {
-        LOGGER.info("Invoking $url");
-
-        CloseableHttpResponse response = null;
-        try {
-            response = httpClient.execute(new HttpGet(url));
-            StatusLine status = response.getStatusLine();
-            LOGGER.info("Successfully invoked $url");
-
-            return new HttpResponse(
-                    url: url,
-                    method: "GET",
-                    statusCode: status?.getStatusCode(),
-                    statusMessage: status?.getReasonPhrase()
-            );
-        }
-        catch (Exception e) {
-            throw new CheckerException("Error occured while invoking $url", e);
-        }
-        finally {
-            response?.close();
-        }
+        return new TimedHttpRequest(url, httpRequestTimeout, httpClient).invoke()
     }
 }
